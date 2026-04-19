0f5a6dbb1d01449c328a0ec1c7d9e2864e0089c7
Redwing47 <Kunal.Narang@iiitb.ac.in>
2026-04-19 19:18:21 +0530

    Added object_exists and started object_write with header and hashing

--- object.c
+++ object.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 // convert hash to hex string
 void hash_to_hex(const ObjectID *id, char *hex_out)
@@ -44,4 +45,51 @@ void compute_hash(const void *data, size_t len, ObjectID *id_out)
     {
         id_out->hash[i % HASH_SIZE] ^= bytes[i];
     }
+}
+
+
+// check if object already exists
+int object_exists(const ObjectID *id)
+{
+    char path[512];
+    object_path(id, path, sizeof(path));
+
+    return access(path, F_OK) == 0;
+}
+
+// start object_write (partial)
+int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out)
+{
+    const char *type_str;
+
+    switch (type)
+    {
+        case OBJ_BLOB: type_str = "blob"; break;
+        case OBJ_TREE: type_str = "tree"; break;
+        case OBJ_COMMIT: type_str = "commit"; break;
+        default: return -1;
+    }
+
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
+
+    size_t total_len = header_len + len;
+    unsigned char *buffer = malloc(total_len);
+    if (!buffer) return -1;
+
+    memcpy(buffer, header, header_len);
+    memcpy(buffer + header_len, data, len);
+
+    compute_hash(buffer, total_len, id_out);
+
+    if (object_exists(id_out))
+    {
+        free(buffer);
+        return 0;
+    }
+
+    // remaining steps in next commit
+
+    free(buffer);
+    return -1;
 }
