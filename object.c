6f455d4ac8168dd995f3abb27777303cc34690c2
Redwing47 <Kunal.Narang@iiitb.ac.in>
2026-04-19 19:06:29 +0530

    Implemented hex_to_hash conversion function

--- object.c
+++ object.c
@@ -12,3 +12,21 @@ void hash_to_hex(const ObjectID *id, char *hex_out)
     }
     hex_out[HASH_HEX_SIZE] = '\0';
 }
+
+int hex_to_hash(const char *hex, ObjectID *id_out)
+{
+    if (strlen(hex) < HASH_HEX_SIZE)
+        return -1;
+
+    for (int i = 0; i < HASH_SIZE; i++)
+    {
+        unsigned int byte_val;
+
+        if (sscanf(hex + i * 2, "%2x", &byte_val) != 1)
+            return -1;
+
+        id_out->hash[i] = (uint8_t)byte_val;
+    }
+
+    return 0;
+}
