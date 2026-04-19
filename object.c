756384b7aa5eb4a230ed7115ad71237509bd56e9
Redwing47 <Kunal.Narang@iiitb.ac.in>
2026-04-19 19:05:11 +0530

    Added pes.h and implemented hash_to_hex helper

--- object.c
+++ object.c
@@ -1,2 +1,14 @@
+#include "pes.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// convert hash to hex string
+void hash_to_hex(const ObjectID *id, char *hex_out)
+{
+    for (int i = 0; i < HASH_SIZE; i++)
+    {
+        sprintf(hex_out + i * 2, "%02x", id->hash[i]);
+    }
+    hex_out[HASH_HEX_SIZE] = '\0';
+}
