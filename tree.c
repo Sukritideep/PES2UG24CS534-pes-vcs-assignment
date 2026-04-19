21a6b641497c7fdee267507c73d8895e6d6bf269
Redwing47 <Kunal.Narang@iiitb.ac.in>
2026-04-19 19:39:09 +0530

    Initialized tree module with includes and mode constants

--- tree.c
+++ tree.c
@@ -1,7 +1,14 @@
+#include "tree.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "index.h"
 
-int main() {
-    printf("Program initialized\n");
-    return 0;
-}
+// forward declaration
+int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
+
+// mode constants
+#define MODE_FILE 0100644
+#define MODE_EXEC 0100755
+#define MODE_DIR 0040000
