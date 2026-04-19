97707ad5823fccde4a38b968344fed195879a089
Redwing47 <Kunal.Narang@iiitb.ac.in>
2026-04-19 19:52:46 +0530

    Implemented index_save with sorting and atomic write

--- index.c
+++ index.c
@@ -57,3 +57,50 @@ int index_load(Index *index)
     fclose(f);
     return 0;
 }
+
+// helper for sorting entries
+static int compare_entries(const void *a, const void *b)
+{
+    return strcmp(((const IndexEntry *)a)->path,
+                  ((const IndexEntry *)b)->path);
+}
+
+int index_save(const Index *index)
+{
+    // sort entries (cast away const)
+    Index *temp = (Index *)index;
+    qsort(temp->entries, temp->count, sizeof(IndexEntry), compare_entries);
+
+    char temp_path[520];
+    snprintf(temp_path, sizeof(temp_path), "%s.tmp", INDEX_FILE);
+
+    FILE *f = fopen(temp_path, "w");
+    if (!f)
+        return -1;
+
+    for (int i = 0; i < index->count; i++)
+    {
+        const IndexEntry *entry = &index->entries[i];
+
+        char hash_hex[HASH_HEX_SIZE + 1];
+        hash_to_hex(&entry->hash, hash_hex);
+
+        fprintf(f, "%o %s %lu %u %s\n",
+                entry->mode,
+                hash_hex,
+                (unsigned long)entry->mtime_sec,
+                entry->size,
+                entry->path);
+    }
+
+    fflush(f);
+    fclose(f);
+
+    if (rename(temp_path, INDEX_FILE) != 0)
+    {
+        remove(temp_path);
+        return -1;
+    }
+
+    return 0;
+}
